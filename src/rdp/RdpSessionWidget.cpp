86c68c538b698d32b6ae69c86c7b15bbb9baac58
ci <ci>
2026-04-23 17:59:21 +0800

    feat: add RdpSessionWidget with HWND embedding and focus handling

--- src/rdp/RdpSessionWidget.cpp
+++ src/rdp/RdpSessionWidget.cpp
@@ -1,6 +1,113 @@
 #include "RdpSessionWidget.h"
 
+#include <QLabel>
+#include <QResizeEvent>
+#include <QTimer>
+
+#include <windows.h>
+
 RdpSessionWidget::RdpSessionWidget(QWidget *parent)
     : QWidget(parent)
 {
+    setAttribute(Qt::WA_NativeWindow);
+    setFocusPolicy(Qt::StrongFocus);
+}
+
+RdpSessionWidget::~RdpSessionWidget()
+{
+    if (m_process)
+        m_process->stop();
+}
+
+void RdpSessionWidget::connectToHost(const QString &exePath,
+                                      const QString &host,
+                                      int port,
+                                      const QString &username,
+                                      const QString &password)
+{
+    if (m_process)
+        m_process->stop();
+
+    m_childWindow = nullptr;
+    m_process = new FreeRdpProcess(this);
+
+    connect(m_process, &FreeRdpProcess::stateChanged,
+            this, &RdpSessionWidget::onStateChanged);
+
+    m_process->start(exePath, host, port, username, password, winId());
+
+    showOverlay("Connecting...");
+}
+
+void RdpSessionWidget::onStateChanged(FreeRdpProcess::State state)
+{
+    emit titleStateChanged(state);
+
+    switch (state) {
+    case FreeRdpProcess::State::Running:
+        QTimer::singleShot(500, this, [this]() {
+            m_childWindow = findChildWindow();
+            if (m_childWindow) {
+                resizeChildWindow();
+                SetFocus(m_childWindow);
+            }
+        });
+        delete m_overlay;
+        m_overlay = nullptr;
+        break;
+    case FreeRdpProcess::State::Finished:
+        m_childWindow = nullptr;
+        showOverlay("Disconnected");
+        break;
+    default:
+        break;
+    }
+}
+
+HWND RdpSessionWidget::findChildWindow() const
+{
+    HWND result = nullptr;
+    EnumChildWindows(reinterpret_cast<HWND>(winId()),
+        [](HWND child, LPARAM lParam) -> BOOL {
+            *reinterpret_cast<HWND*>(lParam) = child;
+            return FALSE;
+        }, reinterpret_cast<LPARAM>(&result));
+    return result;
+}
+
+void RdpSessionWidget::resizeChildWindow()
+{
+    if (!m_childWindow)
+        return;
+    MoveWindow(m_childWindow, 0, 0, width(), height(), TRUE);
+}
+
+void RdpSessionWidget::showOverlay(const QString &text)
+{
+    if (!m_overlay) {
+        m_overlay = new QLabel(text, this);
+        m_overlay->setAlignment(Qt::AlignCenter);
+        m_overlay->setStyleSheet(
+            "QLabel { background: #1e1e1e; color: #cccccc; font-size: 18px; }");
+    } else {
+        m_overlay->setText(text);
+    }
+    m_overlay->setGeometry(0, 0, width(), height());
+    m_overlay->show();
+    m_overlay->raise();
+}
+
+void RdpSessionWidget::resizeEvent(QResizeEvent *event)
+{
+    QWidget::resizeEvent(event);
+    resizeChildWindow();
+    if (m_overlay)
+        m_overlay->setGeometry(0, 0, width(), height());
+}
+
+void RdpSessionWidget::focusInEvent(QFocusEvent *event)
+{
+    QWidget::focusInEvent(event);
+    if (m_childWindow)
+        SetFocus(m_childWindow);
 }
--- src/rdp/RdpSessionWidget.h
+++ src/rdp/RdpSessionWidget.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include <QWidget>
+#include "FreeRdpProcess.h"
 
-// Placeholder - will be implemented in a later task
+class QLabel;
 
 class RdpSessionWidget : public QWidget
 {
@@ -10,4 +11,28 @@ class RdpSessionWidget : public QWidget
 
 public:
     explicit RdpSessionWidget(QWidget *parent = nullptr);
+    ~RdpSessionWidget();
+
+    void connectToHost(const QString &exePath,
+                       const QString &host,
+                       int port,
+                       const QString &username,
+                       const QString &password);
+
+signals:
+    void titleStateChanged(FreeRdpProcess::State state);
+
+protected:
+    void resizeEvent(QResizeEvent *event) override;
+    void focusInEvent(QFocusEvent *event) override;
+
+private:
+    void onStateChanged(FreeRdpProcess::State state);
+    HWND findChildWindow() const;
+    void resizeChildWindow();
+    void showOverlay(const QString &text);
+
+    FreeRdpProcess *m_process = nullptr;
+    QLabel *m_overlay = nullptr;
+    HWND m_childWindow = nullptr;
 };
